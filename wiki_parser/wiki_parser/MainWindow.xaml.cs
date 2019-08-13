cd56594655ae67875cbe4dd366f3da85709e3633
mist-leet <artemov.ilya.r@gmail.com>
2019-08-14 03:28:11 +0500

    add: 	getting picture from wiki 	gitting title from wiki

--- wiki_parser/wiki_parser/MainWindow.xaml.cs
+++ wiki_parser/wiki_parser/MainWindow.xaml.cs
@@ -13,16 +13,75 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+
 namespace wiki_parser
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    // https://en.wikipedia.org/wiki/Saint_Petersburg
+    // <title>Saint Petersburg - Wikipedia</title>
+
     public partial class MainWindow : Window
     {
+        private string FindName(string s)
+        {
+            int start = s.IndexOf("<title>") + 7;
+            int end = s.IndexOf("</title>");
+            if (end > 0)
+                return s.Substring(start, end - start);
+            else
+                return null;
+        }
+
+        private string FindPic(string s)
+        {
+            int start = s.IndexOf("https://upload.wikimedia.org/");
+            if (start > 0)
+            {
+                int i;
+                for (i = start; s[i] != '\"'; i++) ;
+                return s.Substring(start, i - start);
+            }
+            else
+                return null;
+        }
         public MainWindow()
         {
             InitializeComponent();
+            
+            WebRequest req = WebRequest.Create("https://en.wikipedia.org/wiki/Saint_Petersburg");
+            WebResponse res = req.GetResponse();
+
+            WebClient wclient = new WebClient();
+
+            string title = "";
+            string img = "";
+            
+            
+            using (Stream stream = res.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null
+                        && (title == "" || img == ""))
+                    {
+                        if (FindName(line) != null && title == "")
+                            title = FindName(line);
+                        if (FindPic(line) != null && img == "")
+                            img = FindPic(line);
+                    }
+                }
+                wclient.DownloadFile(img, "img.png");
+            }
+
+            main.Text = title;
+            BitmapImage bm = new BitmapImage(new Uri("img.png", UriKind.Relative));
+            image.Source = bm;
+
         }
+
+
     }
 }
